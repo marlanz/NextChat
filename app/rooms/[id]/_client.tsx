15a2e9e69d1a472b40cc3786188bd324b64b993f
marlanz <phucanhdodang1211@gmail.com>
2026-02-01 14:13:01 +0700

    feat: add realtime functionality

--- app/rooms/[id]/_client.tsx
+++ app/rooms/[id]/_client.tsx
@@ -1,14 +1,27 @@
 "use client";
 import ChatInput from "@/components/ChatInput";
+import { ChatMessage } from "@/components/ChatMessage";
+import { createClient } from "@/services/supabase/client";
 import { Message, RoomClientProps } from "@/type";
+import { RealtimeChannel } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
 
 export const RoomClient = ({ room, messages, user }: RoomClientProps) => {
+  const { connectedUsers, messages: realtimeMessages } = useRealtimeChat({
+    roomId: room.id,
+    userId: user.id,
+  });
+
+  const visibleMessages = messages.toReversed().concat(realtimeMessages);
+
   return (
     <div className="container mx-auto h-screen-with-header border border-y-0 flex flex-col">
       <div className="flex items-center justify-between gap-2">
         <div className="p-4 border-b">
           <h1 className="text-2xl font-bold">{room.name}</h1>
-          <p className="text-muted-foreground text-sm">0 users online</p>
+          <p className="text-muted-foreground text-sm">
+            {connectedUsers} users online
+          </p>
         </div>
       </div>
       <div
@@ -19,7 +32,7 @@ export const RoomClient = ({ room, messages, user }: RoomClientProps) => {
         }}
       >
         <div className="">
-          {messages.map((m) => (
+          {visibleMessages.map((m) => (
             <ChatMessage key={m.id} {...m} />
           ))}
         </div>
@@ -33,6 +46,67 @@ function InviteUserModal({ roomId }: { roomId: string }) {
   return <></>;
 }
 
-function ChatMessage(message: Message) {
-  return message.text;
+function useRealtimeChat({
+  roomId,
+  userId,
+}: {
+  roomId: string;
+  userId: string;
+}) {
+  const [connectedUsers, setConnectedUsers] = useState(1);
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    const supabase = createClient();
+    let newChannel: RealtimeChannel;
+    let cancel = false;
+
+    supabase.realtime.setAuth().then(() => {
+      if (cancel) return;
+
+      newChannel = supabase.channel(`room:${roomId}:messages`, {
+        config: {
+          private: true,
+          presence: {
+            key: userId,
+          },
+        },
+      });
+
+      newChannel
+        .on("presence", { event: "sync" }, () => {
+          setConnectedUsers(Object.keys(newChannel.presenceState()).length);
+        })
+        .on("broadcast", { event: "INSERT" }, (payload) => {
+          const record = payload.payload;
+          setMessages((prevMessages) => [
+            ...prevMessages,
+            {
+              id: record.id,
+              text: record.text,
+              created_at: record.created_at,
+              author_id: record.author_id,
+              author: {
+                name: record.author_name,
+                image_url: record.author_image_url,
+              },
+            },
+          ]);
+        })
+        .subscribe((status) => {
+          if (status !== "SUBSCRIBED") return;
+
+          newChannel.track({ userId });
+        });
+    });
+
+    return () => {
+      cancel = true;
+      if (!newChannel) return;
+      newChannel.untrack();
+      newChannel.unsubscribe();
+    };
+  }, [roomId, userId]);
+
+  return { connectedUsers, messages };
 }
